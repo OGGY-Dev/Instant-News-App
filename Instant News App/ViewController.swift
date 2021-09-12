791d6399183e26b56c40d9bcc781ecb5d3816225
Oggy <ogulcandemirtas@outlook.com>
2021-09-12 17:26:23 +0300

    Initial Commit

---
+++ Instant News App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instant News App
+//
+//  Created by Oğulcan DEMİRTAŞ on 12.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
